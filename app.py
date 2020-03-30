04a6c43fe69912f36ca6aa4cb0ab7bc322b802e5
carpet-nagibator <carpet21rus@gmail.com>
2020-03-30 19:11:29 +0300

    35th commit (settings + table)

--- app.py
+++ app.py
@@ -85,17 +85,29 @@ def add_user(viber_id):
         session.close()
 
 
+def add_settings():
+    session = Session()
+    try:
+        session.add(Settings(id_set=1, remind_time=360000, count_words=10, count_to_learn=5))
+        session.commit()
+        session.close()
+    except:
+        session.rollback()
+        session.close()
+
+
 def send_question(viber_id):
     session = Session()
     select_query = session.query(Users.all_answers, Users.correct_answers, Users.user_id,
                                  Users.dt_last_answer).filter(Users.viber_id == viber_id).one()
     session.close()
 
-    session = Session()
-    count_words = session.query(Settings.count_words, Settings.count_to_learn).one()
-    session.close()
+    # session = Session()
+    # count_words = session.query(Settings.count_words, Settings.count_to_learn).filter(Settings.id_set == 1).one()
+    # session.close()
 
-    if select_query[0] >= count_words[0]:
+    #if select_query[0] >= count_words[0]:
+    if select_query[0] >= 10:
         temp_correct_answers = select_query[1]
         session = Session()
         update_query = session.query(Users).filter(Users.viber_id == viber_id).one()
@@ -118,7 +130,8 @@ def send_question(viber_id):
         temp_answers = []
         temp_correct_answer = 100
         question = {}
-        while temp_correct_answer >= count_words[1]:
+        #while temp_correct_answer >= count_words[1]:
+        while temp_correct_answer >= 5:
             question = random.choice(data)
             try:
                 session = Session()
@@ -234,38 +247,38 @@ def hello():
     return render_template('index.html')
 
 
-@app.route('/settings')
-def settings():
-    session = Session()
-    select_query = session.query(Settings.remind_time, Settings.count_words, Settings.count_to_learn).one()
-    session.close()
-    return render_template('settings.html', remind_time=select_query[0], count_words=select_query[1],
-                           count_to_learn=select_query[2])
+# @app.route('/settings')
+# def settings():
+#     session = Session()
+#     select_query = session.query(Settings.remind_time, Settings.count_words, Settings.count_to_learn).one()
+#     session.close()
+#     return render_template('settings.html', remind_time=select_query[0], count_words=select_query[1],
+#                            count_to_learn=select_query[2])
 
 
-@app.route('/accept', methods=['POST'])
-def accept():
-    session = Session()
-    try:
-        session.add(Settings(id_set=1, remind_time=360000, count_words=10, count_to_learn=5))
-        session.commit()
-        session.close()
-    except:
-        session.rollback()
-        session.close()
-
-    remind_time = int(request.form.get('remind_time'))
-    count_words = int(request.form.get('count_words'))
-    count_to_learn = int(request.form.get('count_to_learn'))
-
-    session = Session()
-    update_query = session.query(Settings).one()
-    update_query.remind_time = remind_time
-    update_query.count_words = count_words
-    update_query.count_to_learn = count_to_learn
-    session.commit()
-    session.close()
-    return 'Hello world!'
+# @app.route('/accept', methods=['POST'])
+# def accept():
+#     session = Session()
+#     try:
+#         session.add(Settings(id_set=1, remind_time=360000, count_words=10, count_to_learn=5))
+#         session.commit()
+#         session.close()
+#     except:
+#         session.rollback()
+#         session.close()
+#
+#     remind_time = int(request.form.get('remind_time'))
+#     count_words = int(request.form.get('count_words'))
+#     count_to_learn = int(request.form.get('count_to_learn'))
+#
+#     session = Session()
+#     update_query = session.query(Settings).one()
+#     update_query.remind_time = remind_time
+#     update_query.count_words = count_words
+#     update_query.count_to_learn = count_to_learn
+#     session.commit()
+#     session.close()
+#     return 'Hello world!'
 
 
 with open("english_words.json", "r", encoding='utf-8') as f:
